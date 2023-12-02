cc8848c821b9bfccd55f2473662a6754098a46b9
Matthew Wyant <me@zanfar.com>
2023-12-02 00:59:02 -0700

    Updated Day 2 to OO

--- aoc2023/day02/solution.py
+++ aoc2023/day02/solution.py
@@ -1,45 +1,79 @@
 """AOC 2023 Day 2 solutions."""
 
+from dataclasses import dataclass, field
 from typing import Optional
+
+
+@dataclass
+class RoundTotal:  # noqa: D101
+    r: int = 0
+    g: int = 0
+    b: int = 0
+
+
+@dataclass
+class Round:  # noqa: D101
+    r: int | None = None
+    g: int | None = None
+    b: int | None = None
+
+    @classmethod
+    def parse(cls, line: str) -> "Round":  # noqa: D102
+        data: dict[str, int] = {}
+        colors = [s.strip() for s in line.split(",")]
+        for color in colors:
+            num, name = color.split(" ")
+
+            data[name.strip().lower()[0]] = int(num.strip())
+
+        return cls(**data)
+
+
+@dataclass
+class Game:  # noqa: D101
+    id_: int
+    rounds: list[Round] = field(default_factory=list)
+
+    @classmethod
+    def parse(cls, line: str) -> "Game":  # noqa: D102
+        game, info = line.split(":")
+        game_id = int(game.strip()[5:])
+        rounds = tuple(e.strip() for e in info.split(";"))
+
+        return cls(
+            game_id,
+            [Round.parse(r) for r in rounds],
+        )
+
+    @property
+    def maximums(self) -> RoundTotal:  # noqa: D102
+        return RoundTotal(
+            **{
+                color: max(getattr(rnd, color) or 0 for rnd in self.rounds)
+                for color in ("r", "g", "b")
+            },
+        )
 
 
 def part_a_solution(input_: str) -> Optional[int]:
     """Compute the solution to a Part A input."""
     assert len(input_) > 0
 
-    maximums = {
-        "red": 12,
-        "green": 13,
-        "blue": 14,
-    }
+    maximums = RoundTotal(
+        12,
+        13,
+        14,
+    )
 
-    possible_count = 0
+    possible_total = 0
+    games = [Game.parse(line) for line in input_.splitlines()]
+    for g in games:
+        maxes = g.maximums
 
-    for gamedef in input_.splitlines():
-        gameid, info = gamedef.split(":")
-        possible = True
+        if maxes.r <= maximums.r and maxes.g <= maximums.g and maxes.b <= maximums.b:
+            possible_total += g.id_
 
-        for event in info.split(";"):
-            colors = [s.strip() for s in event.split(",")]
-            for color in colors:
-                num, name = color.split(" ")
-                if int(num) > maximums[name]:
-                    possible = False
-                    break
-
-                if not possible:
-                    break
-
-            if not possible:
-                break
-
-        if not possible:
-            continue
-
-        game_num = int(gameid[5:])
-        possible_count += game_num
-
-    return possible_count
+    return possible_total
 
 
 def part_b_solution(input_: str) -> Optional[int]:
@@ -47,25 +81,11 @@ def part_b_solution(input_: str) -> Optional[int]:
     assert len(input_) > 0
 
     total_power = 0
+    games = [Game.parse(line) for line in input_.splitlines()]
 
-    for gamedef in input_.splitlines():
-        _, info = gamedef.split(":")
-
-        minimums = {
-            "blue": 0,
-            "green": 0,
-            "red": 0,
-        }
-
-        for event in info.split(";"):
-            colors = [s.strip() for s in event.split(",")]
-            for color in colors:
-                num, name = color.split(" ")
-
-                if int(num) > minimums[name]:
-                    minimums[name] = int(num)
-
-        power = minimums["blue"] * minimums["green"] * minimums["red"]
+    for g in games:
+        m = g.maximums
+        power = m.r * m.g * m.b
         total_power += power
 
     return total_power
